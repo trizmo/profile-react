c20b74440533878667f4014129d87c598be12a22
Tristan Perera <tristan.e.perera@gmail.com>
2019-02-11 18:07:47 -0800

    added navigation and other basic components

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  
+  return(
+    <div>
+      This is the home page biatch.
+
+    </div>
+  )
+}
+
+export default Home;
---
+++ src/components/NavBar.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container
+} from 'reactstrap';
+
+
+export default class NavBar extends Component {
+  state = {
+    isOpen: false
+  }
+
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="sm" className="mb-5">
+          <Container>
+            <NavbarBrand href="/"> Tristan Perera | Fullstack Developer </NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+
+              <Nav navbar className="m-auto">
+                <NavItem>
+                  <NavLink href="#"> &lt; &frasl; portfolio &gt; </NavLink>
+                </NavItem>
+              </Nav>
+
+              <Nav navbar className="">
+                <NavItem>
+                  <NavLink href="https://www.linkedin.com/in/tristan-perera/" target="_blank"> &lt; &frasl; LinkedIn &gt; </NavLink>
+                </NavItem>
+              </Nav>
+
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    )
+  }
+}
