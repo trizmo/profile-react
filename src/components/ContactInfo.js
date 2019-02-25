ceea032baf744eea6d79c8106c661b4fc887829b
Tristan Perera <tristan.e.perera@gmail.com>
2019-02-24 22:34:35 -0800

    cleaned up contactInfo

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -6,15 +6,15 @@ import { FaGithub, FaLinkedin, FaFacebook, FaTwitter } from 'react-icons/fa';
 export default function ContactInfo() {
   return (
     <div>
-      <div>
+      <div style={styles.email} className="text-center">
         <h3>tristan.e.perera@gmail.com</h3>        
       </div>
       
-      <div>
+      <div style={styles.phoneNumber} className="text-center">
       <h3>562.555.5555</h3>        
       </div>
 
-        <Row>
+        <Row style={styles.socialMedia} className="text-center">
           <Col>
           <FaGithub style={{fontSize: "2em"}} /> Github
           </Col>
@@ -30,4 +30,15 @@ export default function ContactInfo() {
         </Row>
     </div>
   )
+}
+const styles = {
+  email: {
+    padding: 20,
+  },
+  phoneNumber: {
+    padding: 20,
+  },
+  socialMedia: {
+    padding: 20,
+  },
 }
