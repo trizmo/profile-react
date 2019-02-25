2433e385660e0be97736ecfaa58457afd804043b
Tristan Perera <tristan.e.perera@gmail.com>
2019-02-24 19:03:25 -0800

    cleaned up footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,85 +1,56 @@
 import React from 'react';
-import { Container, Row, Col, NavLink } from 'reactstrap';
+import { Container, Row, Col, NavLink, Button } from 'reactstrap';
 
 const Footer = () => {
   return (
-    // <Footer color="blue" className="font-small pt-4 mt-4">
-    <Container fluid className="text-center text-md-left" style={footerStyles}>
+    <Container fluid className="text-center text-md-left" style={styles.mainFooter}>
+      <Container style={styles.containerStyle}>
+
       <Row>
-        <Col md="6">
-          <h5 className="title text-center">Full Stack React and React Native Developer</h5>
-
-          <Row>
-            Portfolio
-          </Row>
-          <Row>
-            Email | tristan.e.perera@gmail.com
-          </Row>
-          <Row>
-            Email | tristan.e.perera@gmail.com
-          </Row>
-
-
+        <Col>
+          <Button block>GitHub</Button>
         </Col>
-        <Col md="6">
-
-          <Row>
-            <h5 className="title text-center m-auto">Socal Media</h5>
-          </Row>
-
-
-            <Row>
-
-            <Col>
-              <NavLink>LinkedIn</NavLink>
-            </Col>
-            <Col>
-              <NavLink>Twitter</NavLink>
-            </Col>
-            <Col>
-              <NavLink>FaceBook</NavLink>
-            </Col>
-            <Col>
-              <NavLink>InstaGramk</NavLink>
-            </Col>
-      
-            </Row>
-
-            <Row>
-          
-          <Row>
-            562.555.5555
-          </Row>
-
-          <Row>
-            tristan.e.perera@gmail.com
-          </Row>
-          
-            </Row>
-  
-
-          {/* <Row>
-            <h5 className="title text-center m-auto">Contact</h5>
-          </Row> */}
-
-         
-
-
+        <Col>
+          <Button block>LinkedIn</Button>
+        </Col>
+        <Col>
+          <Button block>Twitter</Button>
+        </Col>
+        <Col>
+          <Button block>InstaGram</Button>
         </Col>
       </Row>
+
+      <Row>
+        <Col>
+          <p>tristan.e.perera@gmail.com</p>
+        </Col>
+        <Col>
+          <p>562.555.5555</p>
+        </Col>
+      </Row>
+
+      </Container>
     </Container>
-    // </Footer>
   );
 }
 
-const footerStyles = {
-  position: "fixed",
-  width: "100%",
-  bottom: 0,
-  backgroundColor: "#393D3F",
-  color: "lightgrey",
-  boxShadow: "0px 1px black",
-  marginTop: 1800,
+const styles = {
+  mainFooter: {
+    position: "fixed",
+    width: "100%",
+    bottom: 0,
+    backgroundColor: "#393D3F",
+    color: "lightgrey",
+    boxShadow: "0px 1px black",
+    marginTop: 0,
+    height: 150
+  },
+  containerStyle: {
+    padding: 50,
+    
+  }
+
 }
 
 export default Footer;
