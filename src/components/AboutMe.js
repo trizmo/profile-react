a3b2cee99d143b8996ce754419bf4b26331d9baa
Tristan Perera <tristan.e.perera@gmail.com>
2019-02-26 18:06:05 -0800

    added resume and working download button

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import { Container, Row, Col, Button } from 'reactstrap';
 
+import MyResume from '../data/resume/resume.pdf'
+
 const profilPic = "https://s3-us-west-1.amazonaws.com/co-directory-images/tristan-perera-9ab28682.jpg"
 
 export default class AboutMe extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      resume : MyResume
+    }
+  }
 
   returnProfilePic() {
     return <img src={profilPic} alt="Logo" style={{maxWidth:"500px"}}/>;
@@ -26,9 +35,12 @@ export default class AboutMe extends Component {
               <div>
                 Self-driven web developer with FullStack skills in current industry standards. Passionate about what these technological tools can do to help people achieve their goals, I'm more than eager to problem solve and help innovate. Always looking to better myself, learn new things and to be a valuable asset to any team by bringing my positive energy, and my exceptional problem solving skills
               </div>
-              <Button outline size="lg" color="primary">
+
+              <Button outline size="lg" color="primary" href={this.state.resume} download="Tristan Perera - Software Engineer.pdf">
+                {console.log(MyResume)}
                 Download My Resume
               </Button>
+            
             </Col>
 
             <Col>
