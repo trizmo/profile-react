0ab64d052d35a73f36199356e9cb587306b1bb2e
Tristan Perera <tristan.e.perera@gmail.com>
2019-04-21 16:11:24 -0700

    routing changes

--- server.js
+++ server.js
@@ -14,16 +14,17 @@ mongoose
   .then(() => console.log("Connected to mongoDB..."))
   .catch(err => console.log("ERROR: ", err) )
 
-// ROUTING
+  if(process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"))
+    app.get("/*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    })
+  }
+
+  // ROUTING
 app.use("/api/messages", require("./routes/api/contactMessage"))
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
 
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static("client/build"))
-  app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  })
-}
 
 app.listen(port, () => console.log("Server started on port: ", port))
