9bfdb102ed27011c1c732dcabd2a3d989a76e29a
Tristan Perera <tristan.e.perera@gmail.com>
2019-04-21 17:06:53 -0700

    minor production changes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,11 @@
 const config = require('config');
 const jwt = require('jsonwebtoken');
 
-function auth(req, res, next){
+function auth(req, res, next) {
   const token = req.header('x-auth-token');
 
   // checking for token
-  if(!token) return res.status(401).json({ msg: 'AUTHORIZATION DENIED'});
+  if (!token) return res.status(401).json({ msg: 'AUTHORIZATION DENIED' });
 
   try {
     // if token, then verify it
@@ -13,11 +13,11 @@ function auth(req, res, next){
     // Add user from payload
     req.user = decoded;
     next();
-  } catch(e) {
-    res.status(400).json({ msg: 'token is not valid'})
+  } catch (e) {
+    res.status(400).json({ msg: 'token is not valid' })
 
   }
-  
+
 
 }
 
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ mongoose
 
   if(process.env.NODE_ENV === "production"){
     app.use(express.static("client/build"))
-    app.get("/*", (req, res) => {
+    app.use("/", (req, res) => {
       res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
     })
   }
